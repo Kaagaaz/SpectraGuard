2c3a59e324bbcdabb6035d06bad88a010613459b
_Kaagaaz._ <290294969+Kaagaaz@users.noreply.github.com>
2026-07-25 02:45:58 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,17 +6,17 @@ const scannerAnimation = document.getElementById("scannerAnimation");
 
 const scoreNumber = document.getElementById("scoreNumber");
 
+const scoreCircle = document.querySelector(".score-circle");
 
+
+// Animate privacy score number
 function animateScore(target){
 
     let current = 0;
 
-
     const interval = setInterval(()=>{
 
-
         current++;
-
 
         scoreNumber.innerText = current;
 
@@ -27,6 +27,37 @@ function animateScore(target){
 
         }
 
+    },20);
+
+}
+
+
+
+// Animate score circle
+function updateCircle(target){
+
+    let progress = 0;
+
+
+    const animation = setInterval(()=>{
+
+
+        progress++;
+
+
+        scoreCircle.style.background =
+        `conic-gradient(
+            #00bfff ${progress}%,
+            #222 0%
+        )`;
+
+
+        if(progress >= target){
+
+            clearInterval(animation);
+
+        }
+
 
     },20);
 
@@ -34,36 +65,58 @@ function animateScore(target){
 
 
 
+// Analyze button click
 analyzeBtn.addEventListener("click",()=>{
 
 
-    analyzeBtn.innerText="Scanning...";
+    // Change button
+    analyzeBtn.innerText = "Scanning...";
 
-    analyzeBtn.disabled=true;
+    analyzeBtn.disabled = true;
 
 
-    scannerAnimation.classList.remove("hidden");
 
+    // Reset previous result
 
     results.classList.add("hidden");
 
 
+    // Show scanner animation
+
+    scannerAnimation.classList.remove("hidden");
+
+
+
+    // Simulate scanning time
 
     setTimeout(()=>{
 
 
+        // Hide scanner
+
         scannerAnimation.classList.add("hidden");
 
+
+
+        // Show results
 
         results.classList.remove("hidden");
 
 
+
+        // Start animations
+
         animateScore(82);
-updateCircle();
 
-        analyzeBtn.innerText="Analyze";
+        updateCircle(82);
 
-        analyzeBtn.disabled=false;
+
+
+        // Restore button
+
+        analyzeBtn.innerText = "Analyze";
+
+        analyzeBtn.disabled = false;
 
 
 
