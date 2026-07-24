ca7200a366ff8802f117eabe3ddef694e4445754
_Kaagaaz._ <290294969+Kaagaaz@users.noreply.github.com>
2026-07-25 02:11:44 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
+const analyzeBtn = document.getElementById("analyzeBtn");
+const results = document.getElementById("results");
 
+
+analyzeBtn.addEventListener("click", () => {
+
+    analyzeBtn.innerText = "Scanning...";
+
+    analyzeBtn.disabled = true;
+
+
+    setTimeout(() => {
+
+        results.classList.remove("hidden");
+
+        analyzeBtn.innerText = "Analyze";
+
+        analyzeBtn.disabled = false;
+
+
+    }, 2000);
+
+
+});
